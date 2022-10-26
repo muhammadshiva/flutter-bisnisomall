20b79c9a90c5c8dba7b06d0c955475e9b489fdcd
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-26 10:58:07 +0700

    (payment) : fetch method cubit

--- lib/data/blocs/new_cubit/order/fetch_payment_method/fetch_payment_method_cubit.dart
+++ lib/data/blocs/new_cubit/order/fetch_payment_method/fetch_payment_method_cubit.dart
@@ -4,17 +4,19 @@ import 'package:marketplace/data/models/models.dart';
 import 'package:marketplace/data/repositories/order_repository.dart';
 import 'package:marketplace/data/repositories/repositories.dart';
 
+import '../../../../repositories/new_repositories/transaction_repository.dart';
+
 part 'fetch_payment_method_state.dart';
 
 class FetchPaymentMethodCubit extends Cubit<FetchPaymentMethodState> {
   FetchPaymentMethodCubit() : super(FetchPaymentMethodInitial());
 
-  final OrderRepository _orderRepo = OrderRepository();
+  final TransactionRepository _transactionRepo = TransactionRepository();
 
   Future<void> load() async {
     emit(FetchPaymentMethodLoading());
     try {
-      final response = await _orderRepo.fetchPaymentMethod();
+      final response = await _transactionRepo.fetchPaymentMethod();
       emit(FetchPaymentMethodSuccess(response.data));
     } catch (error) {
       emit(FetchPaymentMethodFailure(error.toString()));
