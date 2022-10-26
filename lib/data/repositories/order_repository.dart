b5486ab8e68b2898b99705a80c8473e3ebaf6d7a
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-26 11:00:11 +0700

    update order repository

--- lib/data/repositories/order_repository.dart
+++ lib/data/repositories/order_repository.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:flutter/foundation.dart';
 import 'package:marketplace/api/api.dart';
-import 'package:marketplace/data/models/checkout.dart';
 import 'package:marketplace/data/models/models.dart';
 import 'package:marketplace/data/models/new_models/tracking.dart';
 import 'package:marketplace/data/repositories/repositories.dart';
@@ -19,18 +18,16 @@ class OrderRepository {
   final String _adsKey = AppConst.API_ADS_KEY;
   Dio dio = new Dio();
 
-  Future<GeneralOrderResponse> addOrder({
-    @required List<int> itemId,
-    @required int recipientId,
-    @required List<String> shippingCode,
-    @required List<int> ongkir,
-    @required List<String> note,
-    @required String verificationMethod,
-    @required int paymentMethodId,
-    int walletLogId,
-    String walletLogToken
-    }) async {
-    
+  Future<GeneralOrderResponse> addOrder(
+      {@required List<int> itemId,
+      @required int recipientId,
+      @required List<String> shippingCode,
+      @required List<int> ongkir,
+      @required List<String> note,
+      @required String verificationMethod,
+      @required int paymentMethodId,
+      int walletLogId,
+      String walletLogToken}) async {
     final token = await _authenticationRepository.getToken();
     var formData = new FormData.fromMap({
       "item_id[]": itemId,
@@ -46,17 +43,14 @@ class OrderRepository {
 
     debugPrint("formdata ${formData.fields}");
 
-   var response = await dio.post(
+    var response = await dio.post(
       "$_baseUrl/order/store",
       data: formData,
-      options: Options(
-        headers: {
-          HttpHeaders.authorizationHeader: 'Bearer $token',
-          HttpHeaders.contentTypeHeader: 'application/json',
-          'ADS-Key':_adsKey
-        },
-        validateStatus: (status) => true
-      ),
+      options: Options(headers: {
+        HttpHeaders.authorizationHeader: 'Bearer $token',
+        HttpHeaders.contentTypeHeader: 'application/json',
+        'ADS-Key': _adsKey
+      }, validateStatus: (status) => true),
     );
 
     // debugPrint("status code ${response.statusCode}");
@@ -73,38 +67,50 @@ class OrderRepository {
     }
   }
 
-  Future<GeneralOrderResponse> addOrderNoAuth({
-    @required String slug,
-    @required String name,
-    @required String phoneNumber,
-    @required String email,
-    @required String address,
-    @required int subdistrictId,
-    @required String verificationMethod,
-    @required int paymentMethodId,
-    @required List<NewCart> carts,
-     List<NoteTemp> notes,
-     List<OngkirTemp> ongkirs,
-     List<ShippingCodeTemp> shppingCodes
-    }) async {    
-
-
-      debugPrint("CHECK NOTES" + notes.toString());
-      debugPrint("CHECK ONGKIRS" + ongkirs.toString());
-      debugPrint("CHECK SHIPPINGCODES" + shppingCodes.toString());
-      debugPrint("CHECK PRODUCTS" + carts[0].product.map((e) => ProductAddOrderNoAuth(productId: e.id, isVariant: e.variantSelected != null ? 1 : 0  , variantId: e.variantSelected != null ? e.variantSelected.id : 0,quantity: e.quantity)).toList().toString());
+  Future<GeneralOrderResponse> addOrderNoAuth(
+      {@required String slug,
+      @required String name,
+      @required String phoneNumber,
+      @required String email,
+      @required String address,
+      @required int subdistrictId,
+      @required String verificationMethod,
+      @required int paymentMethodId,
+      @required List<NewCart> carts,
+      List<NoteTemp> notes,
+      List<OngkirTemp> ongkirs,
+      List<ShippingCodeTemp> shppingCodes}) async {
+    debugPrint("CHECK NOTES" + notes.toString());
+    debugPrint("CHECK ONGKIRS" + ongkirs.toString());
+    debugPrint("CHECK SHIPPINGCODES" + shppingCodes.toString());
+    debugPrint("CHECK PRODUCTS" +
+        carts[0]
+            .product
+            .map((e) => ProductAddOrderNoAuth(
+                productId: e.id,
+                isVariant: e.variantSelected != null ? 1 : 0,
+                variantId: e.variantSelected != null ? e.variantSelected.id : 0,
+                quantity: e.quantity))
+            .toList()
+            .toString());
 
     List<CartTempAddOrderNoAuth> cartsTemp = [];
-    debugPrint("COK 1");
     for (int i = 0; i < carts.length; i++) {
-        cartsTemp.add(CartTempAddOrderNoAuth(
-          supplierId: carts[i].resellerId, 
-          ongkir: ongkirs[i].ongkir, 
-          shippingCode: shppingCodes[i].shippingCode, 
-          note: notes[i].note, 
-          products: carts[i].product.map((e) => ProductAddOrderNoAuth(productId: e.id, isVariant: e.variantSelected != null ? 1 : 0, variantId: e.variantSelected != null ? e.variantSelected.id : 0 , quantity: e.quantity)).toList()));
+      cartsTemp.add(CartTempAddOrderNoAuth(
+          supplierId: carts[i].supplierId,
+          ongkir: ongkirs[i].ongkir,
+          shippingCode: shppingCodes[i].shippingCode,
+          note: notes[i].note,
+          products: carts[i]
+              .product
+              .map((e) => ProductAddOrderNoAuth(
+                  productId: e.id,
+                  isVariant: e.variantSelected != null ? 1 : 0,
+                  variantId:
+                      e.variantSelected != null ? e.variantSelected.id : 0,
+                  quantity: e.quantity))
+              .toList()));
     }
-    debugPrint("COK 2");
     Map<String, dynamic> body = {
       "slug": slug,
       "name": name,
@@ -116,61 +122,13 @@ class OrderRepository {
       "payment_method_id": paymentMethodId,
       "carts": cartsTemp,
     };
-    debugPrint("COK 3");
-    final response = await _provider
-        .post("/order/noauth/store", body: jsonEncode(body), headers: {
-      HttpHeaders.contentTypeHeader: 'application/json',
-      'ADS-Key':_adsKey
-    });
-    debugPrint("COK 4");
-    return GeneralOrderResponse.fromJson(response);
-  }
-
-  Future<PaymentMethodResponse> fetchPaymentMethod() async {
-    final token = await _authenticationRepository.getToken();
-
-    final response = await _provider.get("/payment/show/method", headers: {
-      HttpHeaders.authorizationHeader: 'Bearer $token',
-      HttpHeaders.contentTypeHeader: 'application/json',
-      'ADS-Key':_adsKey
-    });
-    return PaymentMethodResponse.fromJson(response);
-  }
-
-  Future<GeneralOrderResponse> changePaymentMethod({
-    @required int paymentId,
-    @required int paymentMethodId,
-  }) async {
-    var formData = new FormData.fromMap({
-      "payment_method_id": paymentMethodId,
-    });
-    final token = await _authenticationRepository.getToken();
-    var response = await dio.post(
-      "$_baseUrl/payment/change/$paymentId",
-      data: formData,
-      options: Options(
+    final response = await _provider.post("/order/noauth/store",
+        body: jsonEncode(body),
         headers: {
-          HttpHeaders.authorizationHeader: 'Bearer $token',
           HttpHeaders.contentTypeHeader: 'application/json',
-          'ADS-Key':_adsKey
-        },
-        validateStatus: (status) => true
-      ),
-    );
-
-    // debugPrint("status code ${response.statusCode}");
-    debugPrint("response $response");
-
-    if (response.statusCode == 200) {
-      final statusCode = response.data['status'];
-      final message = response.data['message'] ?? 'Terjadi Kesalahan';
-
-      return GeneralOrderResponse.fromJson(response.data);
-    } else {
-      debugPrint("myresponse ${response}");
-      throw GeneralException(response.data.toString());
-    }
-    
+          'ADS-Key': _adsKey
+        });
+    return GeneralOrderResponse.fromJson(response);
   }
 
   Future<GeneralResponse> cancelOrder({@required int paymentId}) async {
@@ -179,7 +137,7 @@ class OrderRepository {
         await _provider.post("/order/$paymentId/cancel-order", headers: {
       HttpHeaders.authorizationHeader: 'Bearer $token',
       HttpHeaders.contentTypeHeader: 'application/json',
-      'ADS-Key':_adsKey
+      'ADS-Key': _adsKey
     });
     return GeneralResponse.fromJson(response);
   }
@@ -195,97 +153,26 @@ class OrderRepository {
     return TrackingOrderResponse.fromJson(response);
   }
 
-  Future<WalletPaymentResponse> orderWithSaldoPanen(
-      {@required int amount}) async {
-    var formData = new FormData.fromMap({
-      "amount": amount,
-    });
-    final token = await _authenticationRepository.getToken();
-    var response = await dio.post(
-      "$_baseUrl/user/wallet/payment",
-      data: formData,
-      options: Options(
-        headers: {
-          HttpHeaders.authorizationHeader: 'Bearer $token',
-          HttpHeaders.contentTypeHeader: 'application/json',
-          'ADS-Key':_adsKey
-        },
-        validateStatus: (status) => true
-      ),
-    );
-
-    debugPrint("response $response");
-
-    if (response.statusCode == 200) {
-      final statusCode = response.data['status'];
-      final message = response.data['message'] ?? 'Terjadi Kesalahan';
-
-      return WalletPaymentResponse.fromJson(response.data);
-    } else {
-      debugPrint("myresponse ${response}");
-      throw GeneralException(response.data.toString());
-    }
-  }
-
-  Future<WalletPaymentResponse> orderWithSaldoPanenConfirmation(
-      {@required int logId,@required int confirmationCode}) async {
-
-    var formData = new FormData.fromMap({
-      "log_id": logId,
-      "confirmation_code":confirmationCode
-    });
-    final token = await _authenticationRepository.getToken();
-    var response = await dio.post(
-      "$_baseUrl/user/wallet/payment/confirm",
-      data: formData,
-      options: Options(
-        headers: {
-          HttpHeaders.authorizationHeader: 'Bearer $token',
-          HttpHeaders.contentTypeHeader: 'application/json',
-          'ADS-Key':_adsKey
-        },
-        validateStatus: (status) => true
-      ),
-    );
-
-    debugPrint("response $response");
-
-    if (response.statusCode == 200) {
-      final statusCode = response.data['status'];
-      final message = response.data['message'] ?? 'Terjadi Kesalahan';
-
-      return WalletPaymentResponse.fromJson(response.data);
-    } else {
-      debugPrint("myresponse ${response}");
-      throw GeneralException(response.data['message'].toString());
-    }
-
-  }
-
-  //======================================================================
-  //======================================================================
-  //======================================================================
-  //======================================================================
-  //======================================================================
-  //======================================================================
-  //======================================================================
-  //======================================================================
-  //======================================================================
-  //======================================================================
-  //======================================================================
-  //======================================================================
-  //======================================================================
-
-  Future<CheckoutResponse> getPaymentOrder({@required orderId}) async {
+  Future<TrackingOrderResponse> fetchTrackingOrderNoAuth(
+      {@required int orderId}) async {
     final token = await _authenticationRepository.getToken();
     final response =
-        await _provider.get("/order/get/$orderId", headers: {
+        await _provider.get("/order/noauth/logs/$orderId", headers: {
       HttpHeaders.authorizationHeader: 'Bearer $token',
       HttpHeaders.contentTypeHeader: 'application/json',
-      'ADS-Key':_adsKey
+      'ADS-Key': _adsKey
     });
-    return CheckoutResponse.fromJson(response);
+    return TrackingOrderResponse.fromJson(response);
   }
 
-  
+  Future<InvoiceByOrderResponse> fetchInvoiceByOrder(
+      {@required int orderId}) async {
+    final token = await _authenticationRepository.getToken();
+    final response = await _provider.get("/order/$orderId/invoice", headers: {
+      HttpHeaders.authorizationHeader: 'Bearer $token',
+      HttpHeaders.contentTypeHeader: 'application/json',
+      'ADS-Key': _adsKey
+    });
+    return InvoiceByOrderResponse.fromJson(response);
+  }
 }
