11bfdae3c2ff102be86fd2d17ee9a62008134748
muhammadshiva <shivamuhammad07@gmail.com>
2022-10-26 10:58:37 +0700

    (payment) : change payment method cubit

--- lib/data/blocs/new_cubit/order/change_payment_method/change_payment_method_cubit.dart
+++ lib/data/blocs/new_cubit/order/change_payment_method/change_payment_method_cubit.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:marketplace/data/models/models.dart';
+import 'package:marketplace/data/repositories/new_repositories/transaction_repository.dart';
 import 'package:marketplace/data/repositories/order_repository.dart';
 import 'package:marketplace/data/repositories/repositories.dart';
 
@@ -10,14 +11,14 @@ part 'change_payment_method_state.dart';
 class ChangePaymentMethodCubit extends Cubit<ChangePaymentMethodState> {
   ChangePaymentMethodCubit() : super(ChangePaymentMethodInitial());
 
-  final OrderRepository _orderRepo = OrderRepository();
+  final TransactionRepository _transactionRepo = TransactionRepository();
 
   Future<void> changePaymentMethod(
       {@required int paymentId, @required int paymentMethodId}) async {
     emit(ChangePaymentMethodLoading());
     try {
       await Future.delayed(Duration(milliseconds: 300));
-      var response = await _orderRepo.changePaymentMethod(
+      var response = await _transactionRepo.changePaymentMethod(
           paymentId: paymentId, paymentMethodId: paymentMethodId);
       emit(ChangePaymentMethodSuccess(response.data));
     } catch (error) {
